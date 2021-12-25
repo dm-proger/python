1dbc4362a66ee1f4e8527114915587e4e5537853
dmytro <shestopalkodmytro@gmail.com>
2021-12-25 20:40:00 +0200

    put on hold

--- homework_shestopalko_dmytro_MAIN/homework_03/task_02.py
+++ homework_shestopalko_dmytro_MAIN/homework_03/task_02.py
@@ -1,13 +1,13 @@
-europe = {
-        "Ukraine": "Kyiv",
-        "Poland": "Warsaw",
-        "Germany": "Berlin",
-        "Italy": "Rome",
-        "France": "Paris",
-        "Spain": "Madrid"
-    }
-
+# europe = {
+#         "Ukraine": "Kyiv",
+#         "Poland": "Warsaw",
+#         "Germany": "Berlin",
+#         "Italy": "Rome",
+#         "France": "Paris",
+#         "Spain": "Madrid"
+#     }
 # def make_country(country_name, capital):
-print(europe["Ukraine"])
-
-
+#
+#
+#
+#
