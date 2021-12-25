dcb0f1990f0d5d1de37db82625f25822dd520aa8
dmytro <shestopalkodmytro@gmail.com>
2021-12-25 20:20:50 +0200

    start od dictionary

---
+++ homework_shestopalko_dmytro_MAIN/homework_03/task_02.py
@@ -0,0 +1,13 @@
+europe = {
+        "Ukraine": "Kyiv",
+        "Poland": "Warsaw",
+        "Germany": "Berlin",
+        "Italy": "Rome",
+        "France": "Paris",
+        "Spain": "Madrid"
+    }
+
+# def make_country(country_name, capital):
+print(europe["Ukraine"])
+
+
