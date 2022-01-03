a104ef05f4106cdf0bfff9748a860548b957e7b2
dmytro <shestopalkodmytro@gmail.com>
2022-01-04 01:47:16 +0200

    sololearn01

--- homework_shestopalko_dmytro_MAIN/homework_0/task_1.py
+++ homework_shestopalko_dmytro_MAIN/homework_0/task_1.py
@@ -1,2 +1,154 @@
 # use ctrl + alt +L fro autoformatting
-print("Hello world")
+# print("Hello world")
+# weight = float(input("Enter your weight: "))
+# height = float(input("Enter your height: "))
+# height *= height
+# bmi = weight / height
+# # print(bmi)
+# if bmi < 18:
+#     print("Underweight")
+# elif 25 > bmi >= 18:
+#     print("Normal")
+# elif 30 > bmi >= 25:
+#     print("Overweight")
+# elif bmi > 30:
+#     print("Obesity")
+# else:
+#     print("The wrong value")
+
+# weight = 130
+# height = 1.7
+# height *= height
+# bmi = weight / height
+#
+# if bmi < 18.5:
+#     print("Underweight")
+# elif 25 > bmi >= 18.5:
+#     print("Normal")
+# elif 30 > bmi >= 25:
+#     print("Overweight")
+# elif bmi > 30:
+#     print("Obesity")
+# else:
+#     print("The wrong value")
+
+# x = [2, 4]
+# x = x * 3
+# print(x)
+# print(x[3])
+
+
+# letters = ["a", "b", "c", "d"]
+# for x in letters:
+#     print(x)
+
+# nums = [4, 7, 3, 1]
+# for x in nums:
+#     print(x*2)
+
+# str = "testing for loops"
+# count = 0
+#
+# for x in str:
+#     if(x == "t"):
+#         count += 1
+#
+# print(count)
+
+# text = "some text"
+# for x in text:
+#     if x == "e":
+#         break
+#     print(x)
+
+# nums = [1, 2, 3, 4]
+# result = 0
+#
+# for x in nums:
+#     if (x %2 == 0):
+#         continue
+#     else:
+#         result += x
+#
+# print(result)
+
+# numbers = list(range(10))
+# print(numbers)
+
+# nums = list(range(3,8))
+# print(nums)
+# print(range(20) == range(0, 20))
+
+# nums = list(range(7, 3, -1))
+# print(nums)
+
+# for i in range(5):
+#     print("hello")
+
+# squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[::2])
+# print(squares[2:8:3])
+
+# sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(sqs[::-1])
+
+# names = [0, 1, 2, 3, 4]
+# print(names[1:-1])
+
+# list = [1, 1, 2, 3, 5, 8, 13]
+# print(list[list[4]])
+
+# list = [1, 2, 3]
+# for let in list:
+#     print(let)
+
+# n = int(input(100))
+# sum = [1, n + 1]
+# in range
+# of
+# sum:
+# print(sum)
+
+# You can iterate over a range and calculate the sum of all numbers in the range.
+# Remember, range(a, b) does not include b, thus you need to use b+1 to include b in the range.
+
+# n = int(input("Enter the number here: "))
+# sum = 0
+# for i in range(0, n + 1):
+#     sum = sum + i
+#     print(sum)
+
+# words = ["Python", "fun"]
+# words.insert(1, "is")
+# print(words)
+# print(words.index("fun"))
+
+# x = [2, 4, 5, 7, 4]
+# y = x.index(4)
+# # print(y)
+# print(min(x))
+# print(max(x))
+
+# x = [2, 4, 6, 2, 7, 2, 9]
+# print(x.count(2))
+#
+# x.remove(4)
+# print(x)
+#
+# x.reverse()
+# print(x)
+
+# str = "some text goes here"
+# x = str.split(" ")
+# print(x)
+
+# txt = "hello"
+# print(max(txt))
+
+
+# the argument is defined inside the parentheses and is named argument_example.
+# def exclamation(argument_example):
+#     print(argument_example + "!")
+#
+#
+# exclamation("spam")
