6e794e8df55b7e322eaad82bad0b8356d8d7e1fb
dmytro <shestopalkodmytro@gmail.com>
2022-01-14 17:02:41 +0200

    temporary commit

--- homework_shestopalko_dmytro_MAIN/homework_11/homework_lms/task_02/adding_new_entries.py
+++ homework_shestopalko_dmytro_MAIN/homework_11/homework_lms/task_02/adding_new_entries.py
@@ -1,8 +1,10 @@
 import json
 
+
 def telephone_function(data, filename="phonebook_dict.json"):
     with open(filename, "w") as f:
         json.dump(data, f)
+
 
 with open("phonebook_dict.json", "r") as json_file:
     data = json.load(json_file)
@@ -10,12 +12,7 @@ with open("phonebook_dict.json", "r") as json_file:
     y = {"name": "Joe", "age": 40}
     temp.append(y)
 
-telephone_function(data)
-
-
-
-
-
+telephone_function(data, filename="phonebook_dict.json")
 
 if __name__ == "__main__":
     main()
