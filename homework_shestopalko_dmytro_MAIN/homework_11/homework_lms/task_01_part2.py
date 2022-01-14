7515991905fa80fe6bb2639b88245ed0253102ce
dmytro <shestopalkodmytro@gmail.com>
2022-01-14 15:10:41 +0200

    task_01 is compleated

---
+++ homework_shestopalko_dmytro_MAIN/homework_11/homework_lms/task_01_part2.py
@@ -0,0 +1,4 @@
+with open("myfile", "r") as file_object:
+    input_var = file_object.read()
+
+print("Nice to see you here, " + input_var + "!")
---
+++ homework_shestopalko_dmytro_MAIN/homework_11/lms/open_dict.py
@@ -0,0 +1,8 @@
+
+import json
+
+with open("cj.json") as file_object:
+    cj = json.load(file_object)
+
+for k, v in cj.items():
+    print(k, ":", v)
