27e3fc9b98cc6a81b6c75aa14085eead592314fc
dmytro <shestopalkodmytro@gmail.com>
2022-01-17 13:24:51 +0200

    commit from the lesson

--- homework_shestopalko_dmytro_MAIN/whatever.py
+++ homework_shestopalko_dmytro_MAIN/whatever.py
@@ -1,10 +1,13 @@
-# k = 0
-# for i in range(10):
-# 	for j in range(3):
-# 		k = k + i
+# def function():
+#     k = 0
+#     for i in range(10):
+#         for j in range(3):
+#             k = k + i
+#         print(i)
+#     print(k)
 #
-# print(k)
-# I can not understand this code!
+# function()
+# I can not understand this code! сума потрійних сум
 
 x = 10
 for i in [1,2,3,4,5]:
