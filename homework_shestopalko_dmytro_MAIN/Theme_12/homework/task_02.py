ef85d772698044304fcff2c34b8cc849b56f5203
dmytro <shestopalkodmytro@gmail.com>
2022-01-17 21:13:36 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Theme_12/homework/task_02.py
+++ homework_shestopalko_dmytro_MAIN/Theme_12/homework/task_02.py
@@ -3,9 +3,11 @@
 # Create a class Dog with class attribute `age_factor` equals to 7.
 # Make __init__() which takes values for a dog’s age.
 # Then create a method `human_age` which returns the dog’s age in human equivalent.
+from typing import ClassVar
+
 
 class Dog():
-    class_attr = 7
+    class_attr: ClassVar[int] = 7
 
     def __init__(self, name: str, human_age: int = 29):
 
