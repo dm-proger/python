bc5a44e0322149160307f0c16fbe080ae5ca0bd5
dmytro <shestopalkodmytro@gmail.com>
2022-01-21 01:17:27 +0200

    semi comit

--- homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_01.py
+++ homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_01.py
@@ -17,27 +17,26 @@ class Person():
 # print(f"My name is {person_01.first_name}, my second name is {person_01.second_name}. I am {person_01.age} years old.")
 
 class Teacher(Person):
-    def __init__(self, salary: str, subject: str, work_hrs: int):
-        super(Teacher, self).__init__(first_name="Maria", second_name="Kovaliuk", age=23)
+    def __init__(self, first_name: str, second_name: str, salary: str, subject: str, work_hrs: int, age: int):
+        super().__init__(first_name, second_name, age)
         self.salary = salary
         self.subject = subject
         self.work_hrs = work_hrs
 
-
-teacher_01 = Teacher(16000, "medicine", 20)
+teacher_01 = Teacher("Maria", "Kovaliuk", 18000, "medicine", 8, 23,)
 print(f"My name is {teacher_01.first_name} {teacher_01.second_name}. I am {teacher_01.age}. "
       f"I work for salary {teacher_01.salary} grn {teacher_01.work_hrs} per week")
 
 
 class Student(Person):
-    def __init__(self, form: str, favorite_subject: str, hobby: str):
-        super(Student, self).__init__(first_name="Volodymyr", second_name="Vynar", age=14)
+    def __init__(self, first_name: str, second_name:str, age: int, form: str, favorite_subject: str, hobby: str):
+        super().__init__(first_name, second_name, age)
         self.form = form
         self.favorite_subject = favorite_subject
         self.hobby = hobby
 
 
-student_01 = Student("7A", "math", "swimming")
-print(
-    f"My name is {student_01.first_name}, my second name is {student_01.second_name}. I am a pupil of {student_01.form}. "
-    f"My favorite subject is {student_01.favorite_subject}, my hobby is {student_01.hobby}")
+student_01 = Student("Volodymyr", "Vynar", 14, "7A", "math", "swimming")
+print(f"My name is {student_01.first_name}, my second name is {student_01.second_name}. I am {student_01.age} years old"
+      f". I am a pupil of {student_01.form}. My favorite subject is {student_01.favorite_subject}."
+      f"My hobby is {student_01.hobby} ")
