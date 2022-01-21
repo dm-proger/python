5e7a0cb4bccf4aa011e2018b9ad8be11a2e2f6ca
dmytro <shestopalkodmytro@gmail.com>
2022-01-22 01:26:38 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/homework_11/homework_lms/task_02/creating the dict.py
+++ homework_shestopalko_dmytro_MAIN/homework_11/homework_lms/task_02/creating the dict.py
@@ -1,15 +1,8 @@
-# Extend Phonebook application
-# Functionality of Phonebook application:
 
-# Add new entries
-# Search by first name
-# Search by last name
-# Search by full name
 # Search by telephone number
 # Search by city or state
 # Delete a record for a given telephone number
 # Update a record for a given telephone number
-# An option to exit the program
 
 # The first argument to the application should be the name of the phonebook.
 # Application should load JSON data, if it is present in the folder with application,
@@ -31,7 +24,7 @@ def search(phonebook_list: list):
         print("This user doesn`t exist")
     else:
         for contact in search_result:
-            print(contact)
+            print(contact["tel_number"])
     phonebook(phonebook_list)
 
 
@@ -41,24 +34,35 @@ def new_user(phonebook_list: list):
     phone_number = int(input("Enter the phone number: "))
     city = input("Enter the city: ")
 
-    person_dict_new_user = {}
-    person_dict_new_user["first_name"] = first_name
-    person_dict_new_user["second_name"] = second_name
-    person_dict_new_user["tel_number"] = phone_number
-    person_dict_new_user["city"] = city
+    new_user = {}
+    new_user["first_name"] = first_name
+    new_user["second_name"] = second_name
+    new_user["tel_number"] = phone_number
+    new_user["city"] = city
 
-    phonebook_list.append(person_dict_new_user)
+    phonebook_list.append(new_user)
     phonebook(phonebook_list)
 
 
+def delete_contact(phonebook_list: list):
+    # select_contact = int(input("Please, select a contact you would like to delete: "))
+    open_phonebook(phonebook_list)
+
+def open_phonebook(phonebook_list:list):
+    with open("phonebook_list.json", "r") as phonebook_file:
+        phonebook_list = json.load(phonebook_file)
+    open_phonebook(phonebook_list)
+
 def phonebook(phonebook_list: Optional[list] = None):
 
-    open_app = int(input(("Find a number = 1\nAdd a new number = 2\nEnter 9 to exit application\nPlease, make your choice: ")))
+    open_app = int(input(("Find a number = 1\nAdd a new number = 2\nDelete a contact = 3\nEnter 9 to exit application\nPlease, make your choice: ")))
 
     if open_app == 1:
         search(phonebook_list)
     elif open_app == 2:
         new_user(phonebook_list)
+    elif open_app == 3:
+        delete_contact(phonebook_list)
     elif open_app == 9:
         with open("phonebook_list.json", "w") as phonebook_file:
             json.dump(phonebook_list, phonebook_file)
@@ -71,8 +75,6 @@ def main():
     with open("phonebook_list.json", "r") as phonebook_file:
         phonebook_list = json.load(phonebook_file)
     phonebook(phonebook_list)
-
-
 
 
 if __name__ == "__main__":
