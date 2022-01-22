50071121c40a1061c32e7930e92d8d4218579960
dmytro <shestopalkodmytro@gmail.com>
2022-01-22 20:40:08 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Theme_12/homework/task_03.py
+++ homework_shestopalko_dmytro_MAIN/Theme_12/homework/task_03.py
@@ -19,42 +19,62 @@
 #
 # CHANNELS = ["BBC", "Discovery", "TV1000"]
 
-class TVController():
+# class TVController():
+#
+#     channels = ["BBC", "Discovery", "TV1000"]
+#
+#     def __init__(self, first_channel, last_channel, turn_channel, next_channel, previous_channel, current_channel, is_exist):
+#         self.first_channel = first_channel
+#         self.last_channel = last_channel
+#         self.turn_channel = turn_channel
+#         self.next_channel = next_channel
+#         self.previous_channel = previous_channel
+#         self.current_channel = current_channel
+#         self.is_exist = is_exist
 
-    channels = ["BBC", "Discovery", "TV1000"]
 
-    def __init__(self, first_channel, last_channel, turn_channel, next_channel, previous_channel, current_channel, is_exist):
-        self.first_channel = first_channel
-        self.last_channel = last_channel
-        self.turn_channel = turn_channel
-        self.next_channel = next_channel
-        self.previous_channel = previous_channel
-        self.current_channel = current_channel
-        self.is_exist = is_exist
+class Buttons():
 
+    def __init__(self, channel_01, channel_02, channel_03, next_channel, previous_channel):
 
-class Buttons(TVController):
-
-    def __init__(self, channel_01, channel_02, channel_03):
         self.channel_01 = channel_01
         self.channel_02 = channel_02
         self.channel_03 = channel_03
+        self.next_channel = next_channel
+        self.previous_channel = previous_channel
 
-watch_tv = Buttons("BBC", "Discovery", "TV1000")
+watch_tv = Buttons("BBC", "Discovery", "TV1000", "BBC", "Discovery")
 
 def main_menu():
     print(f"The following channels are available: 1 - {watch_tv.channel_01}, "
       f"2 - {watch_tv.channel_02}, 3 - {watch_tv.channel_03}")
+
 main_menu()
 
-remote_menu = input("Please, make your choice: ")
+def controller():
+    print("4 - next channel, 5 - previous channel")
+    controller_menu = int(input("Select a command: "))
+    if controller_menu == 4:
+        print(f"You are watching {watch_tv.next_channel}")
+        main_menu()
+    elif controller_menu == 5:
+        print(f" You are watching {watch_tv.previous_channel}")
+        main_menu()
+    else:
+        main_menu()
+
+remote_menu = int(input("Please, make your choice: "))
+
 def menu_bar(remote_menu):
     if remote_menu == 1:
         print(f"You are watching {watch_tv.channel_01}")
+        controller()
     elif remote_menu == 2:
         print(f"You are watching {watch_tv.channel_02}")
+        controller()
     elif remote_menu == 3:
         print(f"You are watching {watch_tv.channel_03}")
+        controller()
     else:
         watch_tv_restart = input("Press 0 to return to the main menu: ")
         if watch_tv_restart == 0:
@@ -62,6 +82,14 @@ def menu_bar(remote_menu):
         else:
             exit()
 menu_bar(remote_menu)
+
+
+
+
+
+
+
+
 
 
 
