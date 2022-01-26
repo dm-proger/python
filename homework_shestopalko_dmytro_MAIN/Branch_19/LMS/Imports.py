fd83729f3ad18020623432f632782fa4ea293635
dmytro <shestopalkodmytro@gmail.com>
2022-01-26 18:43:33 +0200

    semi result

---
+++ homework_shestopalko_dmytro_MAIN/Branch_19/LMS/Imports.py
@@ -0,0 +1,3 @@
+import modules_and_standard_library
+
+print("The fifth fib number is: " + str(modules_and_standard_library.fib(5)))
---
+++ homework_shestopalko_dmytro_MAIN/Branch_19/LMS/imports in python/first.py
@@ -0,0 +1,11 @@
+def(a, b):
+    #sums a and b
+    return a + b
+
+def mul(a, b):
+    #multiply a and b
+    return a * b
+
+def sub(a, b):
+    #substract a and b
+    return a - b
