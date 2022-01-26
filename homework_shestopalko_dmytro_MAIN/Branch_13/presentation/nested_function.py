3a7bb0f2a9486ced71e2bab824b35c94f25071da
dmytro <shestopalkodmytro@gmail.com>
2022-01-26 14:31:51 +0200

    semi commit

--- homework_shestopalko_dmytro_MAIN/Branch_13/presentation/nested_function.py
+++ homework_shestopalko_dmytro_MAIN/Branch_13/presentation/nested_function.py
@@ -1,9 +1,11 @@
 def outer():
     x = 3
-    def inner():
+    def inner(): #it is a nested function
         y = 3
         result = x + y
-        return result
-    return inner
+        return result #outer function returns the inner function
+    return inner # we are returning the reference to the outer inner f-n
 
-outer()
+outer_var = outer() # result of the execution of the outer f-n will be stored in the variable
+print(outer_var())
+print(outer_var.__name__) # we are printing the referenceto the inner function
