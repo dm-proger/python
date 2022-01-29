4dde3b4f2ec500e409a3730d03791c2d319f1abb
dmytro <shestopalkodmytro@gmail.com>
2022-01-29 23:18:16 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_03.py
+++ homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_03.py
@@ -22,27 +22,21 @@
 # assert s.get_product_info("Ramen") == ("Ramen", 290)
 
 
-
 class Product:
-    def __init__(self, type_atr: str, name: str, price: int):
+    def __init__(self, type_atr: str, name: str, price: int, quantity: int):
         self.type_atr = type_atr
         self.name = name
         self.price = price
+        self.quantity = quantity
 
-    def __repr__(self):
-        product_object = product_list
-        return product_object
-
-product_01 = Product("sport", "football T-shirt", 100)
-product_02 = Product("food", "ramen", 1.5)
+product_01 = Product("sport", "football T-shirt", 100, 20)
+product_02 = Product("food", "ramen", 1.5, 30)
 
 product_list = [product_01, product_02]
 
 
-
-class ProductStore(Product):
-    def __init__(self, type_atr: str, name: str, price: int, product_03: str, product_04: str, product_05: str):
-        super().__init__(type_atr, name, price)
+class ProductStore:
+    def __init__(self, product_03: str, product_04: str, product_05: str):
         self.product_01 = product_03
         self.product_02 = product_04
         self.product_03 = product_05
@@ -50,8 +44,7 @@ class ProductStore(Product):
     # # add(product, amount) - adds a specified quantity of a single product
     # #       with a predefined price premium for your store(30 percent)
     def add(self):
-        new_item = product_list.append(self)
-        return new_item
+        ...
     #
     # # set_discount(identifier, percent, identifier_type=’name’) -
     # #   adds a discount for all products specified by input identifiers (type or name).
@@ -79,7 +72,7 @@ class ProductStore(Product):
 
 def main():
     print(f"Here is a category of {product_01.type_atr}, the object is {product_01.name}. The price per piece is {product_01.price}")
-    # print(repr(product_list[0]))
+
 
 if __name__ == "__main__":
     main()
