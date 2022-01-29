cafec824172ba084d515296cc09bfa8741ab4fa6
dmytro <shestopalkodmytro@gmail.com>
2022-01-29 23:51:03 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
+++ homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
@@ -2,14 +2,9 @@
 # 1) Library - name, books = [], authors = []
 # 2) Book - name, year, author (author must be an instance of Author class)
 # 3) Author - name, country, birthday, books = []
-# Library class
 
-# Methods:
-# - new_book(name: str, year: int, author: Author) - returns an instance of Book class and adds the book to the books list for the current library.
-# - group_by_author(author: Author) - returns a list of all books grouped by the specified author
-# - group_by_year(year: int) - returns a list of all the books grouped by the specified year
 # All 3 classes must have a readable __repr__ and __str__ methods.
-# Also, the book class should have a class variable which holds the amount of all existing books
+
 
 class Author():
     def __init__(self, name_auth: str, country: str, birthday: str, books: list):
@@ -24,6 +19,10 @@ print(f"Hello! I am {author_001.name_auth}. My homecountry is {author_001.countr
 
 
 class Book():
+
+    #the book class should have a class variable which holds the amount of all existing books
+    books_list = []
+
     def __init__(self, name_book: str, year: int, author: str):
         self.name_book = name_book
         self.year = year
@@ -35,17 +34,26 @@ class Library():
         self.books = books
         self.authors = authors
 
+    # - new_book(name: str, year: int, author: Author) - returns an instance of Book class
+    # and adds the book to the books list for the current library.
     def new_book(self, name_book: str, year: int, author: Author):
         pass
 
+    # - group_by_author(author: Author) - returns a list of all books grouped by the specified author
     def group_by_author(self, author: Author):
         pass
 
+    # - group_by_year(year: int) - returns a list of all the books grouped by the specified year
     def group_by_year(self, year: int):
         pass
 
 
+    def main():
+        ...
 
+
+if __name__ == "__main__":
+    main()
 
 
 
