99430e776ed64582286063b6adddcd577fd6e5d2
dmytro <shestopalkodmytro@gmail.com>
2022-02-02 01:32:51 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/draft12.py
+++ homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/draft12.py
@@ -1,73 +1,12 @@
-CHANNELS = ["BBC", "Discovery", "TV1000"]
+# Python code to illustrate
+# working of try()
+def divide(x, y):
+	try:
+        # Floor Division : Gives only Fractional Part as Answer
+        result = x // y
+        print("Yeah ! Your answer is :", result)
+	except ZeroDivisionError:
+		print("Sorry ! You are dividing by zero ")
 
-
-class TVController:
-    def __init__(self, channels):
-        self.channels = channels
-        self.count_number = 0
-        self.turned_channel = channels[self.count_number]
-
-    def first_channel(self):
-        return self.channels[0]
-
-    def last_channel(self):
-        return self.channels[-1]
-
-    def turn_channel(self, number):
-        try:
-            self.count_number = (number + 1)
-            self.turned_channel = self.channels[self.count_number]
-            return self.turned_channel
-
-        except IndexError("There is no such a channel") as executed:
-            print(executed)
-
-        except TypeError("Please, enter a number on the remote control") as executed:
-            print(executed)
-
-    def next_channel(self):
-        if self.count_number == len(self.channels) - 1:
-            self.count_number = 0
-        else:
-            self.count_number += 1
-            self.turned_channel = self.channels[self.count_number]
-        return self.turned_channel
-
-    def previous_channel(self):
-        if self.count_number == 0:
-            self.count_number == len(self.channels) - 1
-        else:
-            self.count_number += 1
-        self.turned_channel = self.channels[self.count_number]
-        return self.turned_channel
-
-    def current_channel(self):
-        return self.turned_channel
-
-    def is_exist(self, channel: int | str):
-        try:
-            if type(channel) == int and self.channels[channel - 1]:
-                print("Yes")
-
-            elif type(channel) == str:
-                if channel in self.channels:
-                    print("Yes")
-                else:
-                    print("No")
-
-        except IndexError:
-            print("No")
-
-    def main():
-        controller = TVController(CHANNELS)
-        print(controller.first_channel)
-        print(controller.last_channel())
-        print(controller.turn_channel(1))
-        print(controller.next_channel())
-        print(controller.previous_channel())
-        print(controller.current_channel())
-        controller.is_exist(4)
-        controller.is_exist("BBC")
-
-    if __name__ == "__main__":
-        main()
+# Look at parameters and note the working of Program
+divide(3, 0)
--- homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_03.py
+++ homework_shestopalko_dmytro_MAIN/Branch_13/homework_13/task_03.py
@@ -41,18 +41,15 @@ class ProductStore:
             res += str(i) + ": amount = " + str(self.products[i]) + "; total cost = " + str(i.price * self.products[i]) + "\n"
         return res
 
-# # add(product, amount) - adds a specified quantity of a single product
-# #       with a predefined price premium for your store(30 percent)
+
     def add(self, product: Product, amount: int):
         if product in self.products:
             self.products[product] += amount
         else:
             self.products[product] = amount
         return self.products
-#
-# # set_discount(identifier, percent, identifier_type=’name’) -
-# #   adds a discount for all products specified by input identifiers (type or name).
-#
+
+
     def set_discount(self, identifier: str, percent: float, identifier_type:str = "name"):
         for i in self.products:
             # if identifier_type == "name":
@@ -65,11 +62,16 @@ class ProductStore:
                 i.price -= i.price * percent / 100
 
 
-#
-# # sell_product(product_name, amount) - removes a particular amount of products from the store if available,
-# #        in other case raises an error.
-    def sell_product(self):
-        ...
+    def sell_product(self, product: Product, amount:int):
+        try:
+            if product in self.products:
+                self.products[product] -= amount
+            else:
+                self.products[product] = amount
+            return self.products
+        except NameError:
+            print("There is no such an item")
+
 #
 # # get_income() - returns amount of many earned by ProductStore instance.
     def get_income(self):
@@ -87,15 +89,14 @@ class ProductStore:
 def main():
     product_01 = Product("sport", "football T-shirt", 100)
     product_02 = Product("food", "ramen", 1.5)
-    print(
-        f"Here is a category of {product_01.type_atr}, the object is {product_01.name}. The price per piece is {product_01.price}")
+    print(f"Here is a category of {product_01.type_atr}, the object is {product_01.name}. "
+          f"The price per piece is {product_01.price}")
 
     store = ProductStore({product_01: 3, product_02: 5})
     store.add(Product("food", "bananas", 7), 5)
     store.add(product_02, 5)
-
     store.set_discount("bananas", 10)
-
+    store.sell_product(product_03, 1)
 
     print(store)
 
