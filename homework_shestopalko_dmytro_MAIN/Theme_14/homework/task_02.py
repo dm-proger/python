152ccce46bad17fda673e140879c1f7ff2a2bc88
dmytro <shestopalkodmytro@gmail.com>
2022-02-06 21:21:20 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
+++ homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
@@ -1,11 +1,3 @@
-# Write a class structure that implements a library. Classes:
-# 1) Library - name, books = [], authors = []
-# 2) Book - name, year, author (author must be an instance of Author class)
-# 3) Author - name, country, birthday, books = []
-
-# All 3 classes must have a readable __repr__ and __str__ methods.
-
-
 class Author():
     def __init__(self, name_auth: str, country: str, birthday: str, books: list =[]):
         self.name_auth = name_auth
@@ -43,23 +35,44 @@ class Library():
         self.authors.append(author)
         author.books.append(book_01)
 
-    # - group_by_author(author: Author) - returns a list of all books grouped by the specified author
     def group_by_author(self, author: Author):
-        pass
+        if not isinstance(author, Author):
+            raise TypeError("There is no such author")
+        try:
+            new_book = list(filter(lambda book: book.author == author, self.books))
+            if not new_book:
+                print(f"No books found for author {author.name_auth}")
+            else:
+                print(new_book)
+                return new_book
+        except TypeError as exc:
+            print(exc)
 
     # - group_by_year(year: int) - returns a list of all the books grouped by the specified year
     def group_by_year(self, year: int):
-        pass
+        new_book = list(filter(lambda book: book.year == year, self.books))
+        if new_book == []:
+            print(f"No books found by year {year}")
+        else:
+            print(new_book)
+            return new_book
 
 
 def main():
     author_001 = Author("Mark Zukerberg", "USA", "11.11.1990")
-    print(f"Hello! I am {author_001.name_auth}. My homecountry is {author_001.country}. My birthday is on {author_001.birthday}."
-          f"I wrote a book {author_001.books}")
+    author_002 = Author("Tim Cook", "USA", "12.12.1991")
+    author_003 = Author("John Kennedy", "USA", "10.10.1991")
+    # print(f"Hello! I am {author_001.name_auth}. My homecountry is {author_001.country}. My birthday is on {author_001.birthday}."
+    #       f"I wrote a book {author_001.books}")
 
-    library_01 = Library("Name of the library")
-    library_01.new_book("Name of a new book", 2020, author_001)
-    print(library_01.books, library_01.authors)
+    library_01 = Library("Central Library")
+    library_01.new_book("Name of a new book_01", 2020, author_001)
+    library_01.new_book("Name of a new book_0101", 2020, author_001)
+    library_01.new_book("Second name of a book", 2021, author_002)
+    library_01.new_book("Third name of a book", 2020, author_003)
+
+    print(library_01.group_by_author(author_001))
+    # print(library_01.books, library_01.authors)
 
 if __name__ == "__main__":
     main()
