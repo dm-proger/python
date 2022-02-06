b49fc8025f574d49e127c9d2f7ffbbdac143f0a3
dmytro <shestopalkodmytro@gmail.com>
2022-02-06 20:15:36 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
+++ homework_shestopalko_dmytro_MAIN/Theme_14/homework/task_02.py
@@ -7,37 +7,41 @@
 
 
 class Author():
-    def __init__(self, name_auth: str, country: str, birthday: str, books: list):
+    def __init__(self, name_auth: str, country: str, birthday: str, books: list =[]):
         self.name_auth = name_auth
         self.country = country
         self.birthday = birthday
         self.books = books
 
-author_001 = Author("Mark Zukerberg", "USA", "11.11.1990", "How to become a billionaire.")
-print(f"Hello! I am {author_001.name_auth}. My homecountry is {author_001.country}. My birthday is on {author_001.birthday}."
-      f"I wrote a book {author_001.books}")
-
+    def __repr__(self):
+        return self.name_auth
 
 class Book():
-
-    #the book class should have a class variable which holds the amount of all existing books
     books_list = []
 
-    def __init__(self, name_book: str, year: int, author: str):
+    def __init__(self, name_book: str, year: int, author: Author):
         self.name_book = name_book
         self.year = year
         self.author = author
 
+    def __repr__(self):
+        return f"{self.name_book}, {self.year}"
+
 class Library():
-    def __init__(self, library_name: str, books: list, authors: list):
+    def __init__(self, library_name: str, books: list = [], authors: list = []):
         self.library_name = library_name
         self.books = books
         self.authors = authors
 
-    # - new_book(name: str, year: int, author: Author) - returns an instance of Book class
-    # and adds the book to the books list for the current library.
-    def new_book(self, name_book: str, year: int, author: Author):
-        pass
+    def __repr__(self):
+        return self.books
+
+
+    def new_book(self, book_name: str, book_year: int, author:Author):
+        book_01 = Book(name_book=book_name, year=book_year, author=author)
+        self.books.append(book_01)
+        self.authors.append(author)
+        author.books.append(book_01)
 
     # - group_by_author(author: Author) - returns a list of all books grouped by the specified author
     def group_by_author(self, author: Author):
@@ -48,9 +52,14 @@ class Library():
         pass
 
 
-    def main():
-        ...
+def main():
+    author_001 = Author("Mark Zukerberg", "USA", "11.11.1990")
+    print(f"Hello! I am {author_001.name_auth}. My homecountry is {author_001.country}. My birthday is on {author_001.birthday}."
+          f"I wrote a book {author_001.books}")
 
+    library_01 = Library("Name of the library")
+    library_01.new_book("Name of a new book", 2020, author_001)
+    print(library_01.books, library_01.authors)
 
 if __name__ == "__main__":
     main()
