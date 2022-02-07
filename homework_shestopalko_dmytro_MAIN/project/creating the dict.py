fb2a377b3a56ba20a35e490a92efc1c884505a00
dmytro <shestopalkodmytro@gmail.com>
2022-02-08 00:27:57 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/project/creating the dict.py
+++ homework_shestopalko_dmytro_MAIN/project/creating the dict.py
@@ -1,18 +1,19 @@
 import json
 
-dict_employee = {} # the whole dictionary. Holds all the info
-dict_employee_key_name = {} # holds names as keys and credentials as position and rate
-dict_employee_value = {} # holds position and rate
+dict_employee = {} # holds names as keys; dict_employee_value as value
+dict_employee_value = {} # holds position as a value and rate as a key
 
 
 def add_employee():
-    manager_input_full_name = input("Enter the full name: ")
-    manager_input_position_key = input("Enter the position of the employee: ")
-    manager_input_rate_value = int(input("Enter the rate: "))
-    dict_employee_value.update({manager_input_position_key: manager_input_rate_value})
-    dict_employee_key_name.update({manager_input_full_name: dict_employee_value})
+    input_full_name = input("Enter the full name: ")
+    input_position_key = input("Enter the position of the employee: ")
+    input_rate_value = int(input("Enter the rate: "))
 
-    print(dict_employee_key_name)
+    dict_employee.update({input_full_name: dict_employee_value})
+    dict_employee_value.update({input_position_key: input_rate_value})
+
+
+    print(dict_employee)
 
 def function_write(dict_employee: dict):
     with open("employee_info.json", "w") as file_object:
