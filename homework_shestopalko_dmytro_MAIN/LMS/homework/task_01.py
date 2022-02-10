18d0f09032ff6d659dcba3b45162ee8750785e39
dmytro <shestopalkodmytro@gmail.com>
2022-02-10 23:06:29 +0200

    semi result

---
+++ homework_shestopalko_dmytro_MAIN/LMS/homework/task_01.py
@@ -0,0 +1,4 @@
+# A bubble sort can be modified to “bubble” in both directions.
+# The first pass moves “up” the list and the second pass moves “down.”
+# This alternating pattern continues until no more passes are necessary.
+# Implement this variation and describe under what circumstances it might be appropriate.
