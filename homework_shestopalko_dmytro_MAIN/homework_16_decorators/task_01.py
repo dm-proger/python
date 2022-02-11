952f0b83fdc9af0ffd4294cd56b42a2d2792ab35
dmytro <shestopalkodmytro@gmail.com>
2022-02-11 22:54:52 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/homework_16_decorators/task_01.py
+++ homework_shestopalko_dmytro_MAIN/homework_16_decorators/task_01.py
@@ -3,25 +3,29 @@
 # For example:
 #  "add called with 4, 5"
 
+from functools import wraps
+
+
 def logger(func):
-    def wrapper(x = 10, y = 90):
-        if x+y > -3:
-            print("All good")
-        else:
-            print("All not good")
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        args_as_string = [repr(a) for a in args]
+        kwargs_as_string = [f"{k}={v}" for k, v in kwargs.items()]
+        print(f"{func_name} called with {(', ').join(args_as_string + kwargs_as_string)}")
+        return func(*args, **kwargs)
     return wrapper
 
-
-
+# if there are arguments, the output is correct. Modify the f-n for the output without arguments.
 @logger
 def add(x, y):
-    return x + y
+    print(x + y)
 
-add()
+add(10, 20)
 
 @logger
 def square_all(*args):
     list_square_all = [arg ** 2 for arg in args]
-    return list_square_all
+    print(list_square_all)
 
-square_all()
+square_all(10, 20)
