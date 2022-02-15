c29d65cf5f6fc635665cee70cf75cdf5ea1fadcd
dmytro <shestopalkodmytro@gmail.com>
2022-02-15 23:51:40 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/project/creating_the_dict.py
+++ homework_shestopalko_dmytro_MAIN/project/creating_the_dict.py
@@ -1,9 +1,7 @@
 import json
-from project.temp_folder.temp_2 import my_temp_func
 
 
-
-dict_employee = {} # holds names as keys; dict_employee_value as value
+list_employee = [] # list of employees with all the data
 dict_employee_value = {} # holds position as a value and rate as a key
 
 
@@ -12,18 +10,15 @@ def add_employee():
     input_position_key = input("Enter the position of the employee: ")
     input_rate_value = int(input("Enter the rate: "))
 
-    # dict_employee.update({input_full_name: dict_employee_value})
-    # dict_employee_value.update({input_position_key: input_rate_value})
-
-    dict_employee[input_full_name] = dict_employee_value
+    list_employee[input_full_name] = dict_employee_value
     dict_employee_value[input_position_key] = input_rate_value
 
 
-    function_write(dict_employee)
+    function_write(list_employee)
 
-def function_write(dict_employee: dict):
+def function_write(list_employee: dict):
     with open("employee_info.json", "w") as file_object:
-        json.dump(dict_employee, file_object, indent=4)
+        json.dump(list_employee, file_object, indent=4)
 
 
 
@@ -31,7 +26,7 @@ def main():
 
 
     with open("employee_info.json", "r") as file_object:
-        dict_employee = json.load(file_object)
+        list_employee = json.load(file_object)
 
 
     add_employee()
