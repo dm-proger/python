615581ffe453c1bf4b15bb08aaf075fa9b31b986
dmytro <shestopalkodmytro@gmail.com>
2022-02-16 00:36:48 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/project/creating_the_dict.py
+++ homework_shestopalko_dmytro_MAIN/project/creating_the_dict.py
@@ -2,7 +2,12 @@ import json
 
 
 list_employee = [] # list of employees with all the data
-dict_employee_value = {} # holds position as a value and rate as a key
+dict_employee = {} # holds position as a key and rate as value
+dict_data = {} # holds name as a key and dict_employee as value
+
+
+def main_menu():
+#     describe the main menu. Select the option
 
 
 def add_employee():
@@ -10,13 +15,14 @@ def add_employee():
     input_position_key = input("Enter the position of the employee: ")
     input_rate_value = int(input("Enter the rate: "))
 
-    list_employee[input_full_name] = dict_employee_value
-    dict_employee_value[input_position_key] = input_rate_value
-
+    dict_data.update({input_position_key: input_rate_value})
+    dict_employee.update({input_full_name: dict_data})
+    list_employee.append(dict_employee)
 
     function_write(list_employee)
 
-def function_write(list_employee: dict):
+
+def function_write(list_employee: list):
     with open("employee_info.json", "w") as file_object:
         json.dump(list_employee, file_object, indent=4)
 
