4109722299a32c16cce0c54b0bf1dfb9eb9c5aeb
dmytro <shestopalkodmytro@gmail.com>
2022-02-19 19:19:20 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/project/calling_the_list_employee.py
+++ homework_shestopalko_dmytro_MAIN/project/calling_the_list_employee.py
@@ -7,4 +7,12 @@ for k, v in cj.items():
     print(k, ":", v)
 
 
+with open("phonebook_list.json", "r") as phonebook_file:
+    phonebook_list = json.load(phonebook_file)
+
+# for k, v in phonebook_list.items():
+#     print(k, ":", v)
+print(*employee_info, sep = '\n')
+
+
 
