25df843c37ed70fa8cbf00030e738291864854f0
dmytro <shestopalkodmytro@gmail.com>
2022-02-22 15:32:05 +0200

    semi result

--- homework_shestopalko_dmytro_MAIN/project/main_file.py
+++ homework_shestopalko_dmytro_MAIN/project/main_file.py
@@ -1,23 +1,42 @@
 import json
 
 
-list_employee = [] # list of dicts with employees data
+list_employee = ["All info is here"] # list of dicts with employees data
 employee_data = {} # holds key and value of employees
 
 
-def main_menu():
-    main_enter = int(input("If you are a user, please, enter 1. If you are a manager, please, enter 0. "
-                           "Exit - enter 9: "))
+def main_menu(list_employee: list):
+    main_enter = int(input("If you are a user, please, enter 1.\nIf you are a manager, please, enter 0.\n"
+                           "Exit - enter 9.\nSelect the option:  "))
     if main_enter == 1:
         print("Here is the main interface for you")
         main_interface()
     elif main_enter == 0:
-        manager_function()
+        manager_function(list_employee)
     elif main_enter == 9:
         exit()
     else:
         print("Please, try again")
         main_menu()
+
+
+def manager_function(list_employee: list):
+    manager_input = int(input("Add employee - 1,\nRemove employee - 2,\nChange rate - 3,\n"
+                              "Change position - 4,\nExit - 9\n"
+                              "Please, select from above: "))
+    if manager_input == 1:
+        add_employee()
+    elif manager_input == 2:
+        remove_employee()
+    elif manager_input == 3:
+        change_rate()
+    elif manager_input == 4:
+        change_position()
+    elif manager_input == 9:
+        exit()
+    else:
+        print("Please, try again")
+        main_menu(list_employee)
 
 
 def add_employee():
@@ -31,11 +50,9 @@ def add_employee():
     employee_data["rate"] = input_rate
 
     list_employee.append(employee_data)
-
-
     function_write(list_employee)
     print("The employee is successfully added")
-    manager_function()
+    manager_function(list_employee)
 
 
 
@@ -51,25 +68,6 @@ def change_position():
 def main_interface():
     ...
 
-def manager_function():
-    manager_input = int(input("Add employee - 1, remove employee - 2, change rate - 3, "
-                              "change position - 4, exit - 9 "
-                              "Please, select from above: "))
-    if manager_input == 1:
-        add_employee()
-    elif manager_input == 2:
-        remove_employee()
-    elif manager_input == 3:
-        change_rate()
-    elif manager_input == 4:
-        change_position()
-    elif manager_input == 9:
-        exit()
-    else:
-        print("Please, try again")
-        main_menu()
-
-
 
 def function_write(list_employee: list):
     with open("employee_info.json", "w") as file_object:
@@ -81,9 +79,9 @@ def main():
 
     with open("employee_info.json", "r") as file_object:
         list_employee = json.load(file_object)
-    main_menu()
 
-    main_menu()
+    main_menu(list_employee)
+
 
 
 if __name__ == "__main__":
